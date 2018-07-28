d42d21494e79937b3c7aaa84b1297872b65a8552
161250103 <1767855130@qq.com>
2018-07-28 14:01:28 +0800

    create back end project

---
+++ bequank/src/main/java/com/belfry/bequank/BequankApplication.java
@@ -0,0 +1,12 @@
+package com.belfry.bequank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BequankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BequankApplication.class, args);
+    }
+}
