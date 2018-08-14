396c8ebfc9c7ec467ecb74f4255b6fb5a80ea415
Unknown <1767855130@qq.com>
2018-08-14 18:29:18 +0800

    add the 2 files

---
+++ bequank/src/main/java/com/belfry/bequank/util/Message.java
@@ -0,0 +1,13 @@
+package com.belfry.bequank.util;
+
+public class Message {
+    public static final int MSG_SUCCESS         = 200 ;
+
+    public static final int MSG_EMAIL_FAILED  = 417 ;   //发送验证码失败
+    public static final int MSG_WRONG_VERICODE  = 418 ; //验证码错误
+    public static final int MSG_DUPLICATE_EMAIL = 419 ; //用户名重复
+
+    public static final int MSG_WRONG_PASSWORD  = 420 ; //密码错误
+    public static final int MSG_USER_NOTEXIST   = 421 ; //用户不存在
+    
+}
---
+++ bequank/src/main/java/com/belfry/bequank/util/Role.java
@@ -0,0 +1,6 @@
+package com.belfry.bequank.util;
+
+public class Role {
+    public static final String NORMAL="NORMAL";
+
+}
