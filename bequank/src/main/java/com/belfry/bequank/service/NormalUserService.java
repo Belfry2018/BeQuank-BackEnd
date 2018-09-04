55b30735d42a44a260ab7287b1ff7c7bf4847724
Unknown <1767855130@qq.com>
2018-09-04 15:26:23 +0800

    多因子接口确定

--- bequank/src/main/java/com/belfry/bequank/service/NormalUserService.java
+++ bequank/src/main/java/com/belfry/bequank/service/NormalUserService.java
@@ -1,7 +1,13 @@
 package com.belfry.bequank.service;
 
+import net.sf.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 普通用户api
  */
 public interface NormalUserService {
+
+    public JSONObject getQuestionnairResult(HttpServletRequest request, JSONObject answer);
 }
--- bequank/src/main/java/com/belfry/bequank/serviceImpl/NormalUserServiceImpl.java
+++ bequank/src/main/java/com/belfry/bequank/serviceImpl/NormalUserServiceImpl.java
@@ -1,7 +1,14 @@
 package com.belfry.bequank.serviceImpl;
 
+import com.belfry.bequank.service.NormalUserService;
 import com.belfry.bequank.service.UserService;
+import jdk.nashorn.internal.ir.IfNode;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sun.security.x509.CRLDistributionPointsExtension;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author: Yang Yuqing
@@ -10,7 +17,42 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Modifiedby:
  */
 
-public class NormalUserServiceImpl {
+@Service
+public class NormalUserServiceImpl implements NormalUserService {
     @Autowired
     UserService userService;
+
+    private int[][]points={{1,4,7,8,9},{2,4,6,7,9},{9,7,3,1,0},{2,6,8,9,0},
+                                {1,4,8,9,0},{1,3,7,9,0},{1,3,7,9,0},{3,6,7,9,0},
+                                {3,5,7,9,0},{1, 3, 5, 7, 0}};
+
+    @Override
+    public JSONObject getQuestionnairResult(HttpServletRequest request, JSONObject answer) {
+        JSONObject res = new JSONObject();
+        int score = 0;
+        for (int i = 1; i < 11; i++) {
+            char op = answer.getString(Integer.toString(i)).charAt(0);
+            score += points[i - 1][op - 'A'];
+        }
+        res.put("score", score);
+        if (score >= 70) {
+            res.put("type", "进取型");
+        }
+        if (score >= 62) {
+            res.put("type", "中度进取型");
+        }
+        if (score >= 54) {
+            res.put("type", "平稳型");
+        }
+        if (score >= 46) {
+            res.put("type", "中度保守型");
+        }
+        if (score >= 39) {
+            res.put("type", "保守型");
+        } else {
+            res.put("type", "不建议投资");
+        }
+
+        return res;
+    }
 }
