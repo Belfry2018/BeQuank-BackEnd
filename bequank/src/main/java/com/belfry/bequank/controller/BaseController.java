fdea14ccd5e90a6819631e9aa44679bbf6dbbafc
Unknown <1767855130@qq.com>
2018-09-07 15:01:33 +0800

    add /api/v1

--- bequank/src/main/java/com/belfry/bequank/controller/BaseController.java
+++ bequank/src/main/java/com/belfry/bequank/controller/BaseController.java
@@ -10,10 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +18,7 @@ import java.io.*;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/v1")
 public class BaseController {
 
     @Value("${belfry.expiration_time}")
