09f15acdb804fc65f56d067c9b1dfb236c6e49b6
sinx-wang <cosx1999@hotmail.com>
2018-09-09 22:53:57 +0800

    政府热词上线

--- bequank/src/main/java/com/belfry/bequank/repository/secondary/Word_tfRepository.java
+++ bequank/src/main/java/com/belfry/bequank/repository/secondary/Word_tfRepository.java
@@ -3,7 +3,6 @@ package com.belfry.bequank.repository.secondary;
 import com.belfry.bequank.entity.secondary.Word_tf;
 import com.belfry.bequank.entity.secondary.Word_tf_PK;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +15,6 @@ import java.util.List;
 @Repository
 public interface Word_tfRepository extends JpaRepository<Word_tf, Word_tf_PK> {
 
+    //TODO: 需要更改，可能返回多值
     List<Word_tf> findByWord(String word);
 }
