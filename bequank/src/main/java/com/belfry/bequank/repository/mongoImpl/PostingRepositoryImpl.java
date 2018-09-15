442dc9f26563557b6b3c10270a7d15e6e24d6dab
AndyLeecs <161250055@smail.nju.edu.cn>
2018-09-16 00:01:30 +0800

    筛选掉有空项的微博

--- bequank/src/main/java/com/belfry/bequank/repository/mongoImpl/PostingRepositoryImpl.java
+++ bequank/src/main/java/com/belfry/bequank/repository/mongoImpl/PostingRepositoryImpl.java
@@ -5,6 +5,8 @@ import com.belfry.bequank.repository.mongo.PostingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
@@ -18,12 +20,20 @@ public class PostingRepositoryImpl implements PostingRepository {
 
     @Override
     public ArrayList<Posting> getHotSpots(int page, int count) {
-        Query q = new Query();
-//      q.with(new Sort(Sort.Direction.DESC, "created_at"));
-        q.addCriteria(Criteria.where("avatar").ne("").ne(null));
-        q.with(new Sort(Sort.Direction.DESC, "attitudes_count", "comments_count", "reposts_count"));
-        q.skip((page - 1) * count).limit(count);
-        ArrayList<Posting> res = (ArrayList<Posting>) mongoTemplate.find(q, Posting.class);
+        Aggregation aggregation = Aggregation.newAggregation(
+                Aggregation.match(Criteria.where("avatar").ne("").ne(null)),
+                Aggregation.sort(new Sort(Sort.Direction.DESC, "attitudes_count", "comments_count", "reposts_count")),
+                Aggregation.skip((page - 1) * count),
+                Aggregation.limit(count)
+        );
+        AggregationResults<Posting> aggRes = mongoTemplate.aggregate(aggregation, "weibos",
+                Posting.class);
+        ArrayList<Posting> res = (ArrayList<Posting>) aggRes.getMappedResults();
+//        Query q = new Query();
+//        q.addCriteria(Criteria.where("avatar").ne("").ne(null));
+//        q.with(new Sort(Sort.Direction.DESC, "attitudes_count", "comments_count", "reposts_count"));
+//        q.skip((page - 1) * count).limit(count);
+//        ArrayList<Posting> res = (ArrayList<Posting>) mongoTemplate.find(q, Posting.class);
         //add avatar
 //        for (int i = 0; i < res.size(); i++) {
 //            Posting posting = res.get(i);
