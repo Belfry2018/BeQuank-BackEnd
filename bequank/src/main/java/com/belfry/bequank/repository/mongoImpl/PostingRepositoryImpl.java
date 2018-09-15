4597d9e816e9c368bfd0c6d6b703cfc05d57730c
AndyLeecs <161250055@smail.nju.edu.cn>
2018-09-15 12:30:09 +0800

    获得clean text

--- bequank/src/main/java/com/belfry/bequank/repository/mongoImpl/PostingRepositoryImpl.java
+++ bequank/src/main/java/com/belfry/bequank/repository/mongoImpl/PostingRepositoryImpl.java
@@ -1,12 +1,10 @@
 package com.belfry.bequank.repository.mongoImpl;
 
 import com.belfry.bequank.entity.mongo.Posting;
-import com.belfry.bequank.entity.mongo.Users;
 import com.belfry.bequank.repository.mongo.PostingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
@@ -21,19 +19,21 @@ public class PostingRepositoryImpl implements PostingRepository {
     public ArrayList<Posting> getHotSpots(int page, int count) {
         Query q = new Query();
 //      q.with(new Sort(Sort.Direction.DESC, "created_at"));
+//        q.addCriteria(Criteria.where("text").ne("").ne(null));
         q.with(new Sort(Sort.Direction.DESC, "attitudes_count", "comments_count", "reposts_count"));
         q.skip((page - 1) * count).limit(count);
         ArrayList<Posting> res = (ArrayList<Posting>) mongoTemplate.find(q, Posting.class);
         //add avatar
-        for (int i = 0; i < res.size(); i++) {
-            Posting posting = res.get(i);
-            String name = posting.getUser();
-            Query query = new Query();
-            query.addCriteria(Criteria.where("name").is(name));
-            Users user = mongoTemplate.findOne(query, Users.class);
-            String avatar = user.getAvatar();
-            posting.setAvatar(avatar);
-        }
+//        for (int i = 0; i < res.size(); i++) {
+//            Posting posting = res.get(i);
+//            String name = posting.getUser();
+//            long id = Long.parseLong(name);
+//            Query query = new Query();
+//            query.addCriteria(Criteria.where("id").is(id));
+//            Users user = mongoTemplate.findOne(query, Users.class);
+//            String avatar = user.getAvatar();
+//            posting.setAvatar(avatar);
+//        }
         return res;
     }
 
