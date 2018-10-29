71f2b17c4b024af01ef43267c20bd971c2826442
frostwing98 <498924217@qq.com>
2018-10-29 23:21:20 +0800

    update controller 3 methods

--- bequank/src/main/java/com/belfry/bequank/repository/primary/CommentRepository.java
+++ bequank/src/main/java/com/belfry/bequank/repository/primary/CommentRepository.java
@@ -3,6 +3,7 @@ package com.belfry.bequank.repository.primary;
 import com.belfry.bequank.entity.primary.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +18,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     @Query("select m from Comment m")
     List<Comment> getAll();
+    @Query("select m from Comment m where m.replyTargetUserid=:userid and m.alreadyread=0")
+    List<Comment> getUnreadReplies(@Param("uesrid")Long userid);
 }
